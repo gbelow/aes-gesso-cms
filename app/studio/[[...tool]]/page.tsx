33d60cee24d3e9685d78d97c59448ec616ce7eb8
gbelow <guilherme.below@hotmail.com>
2026-02-05 10:32:58 +0100

    setting for cloudflare

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -9,6 +10,10 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+
+export function generateStaticParams() {
+  return [];
+}
 
 export const dynamic = 'force-static'
 
--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,14 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
+    unoptimized: true, // REQUIRED for Cloudflare Pages
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         port: '',
         pathname: '/**',
+        
       },
     ],
   },
